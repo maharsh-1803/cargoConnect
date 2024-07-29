1215b2a2ed1bf87490bfbde4523ee3489fb7fd98
maharsh-1803 <maharshpatel1803@gmail.com>
2024-07-29 21:50:17 +0530

    route

--- route/truckAgency.route.js
+++ route/truckAgency.route.js
@@ -1,10 +1,11 @@
 const { addAgency, loginAgencyOwner, getAgencyById } = require('../controller/truckAgency.controller');
+const AuthToken = require('../middleware/authToken');
 const upload = require('../middleware/multer.middleware');
 
 const router = require('express').Router();
 
 router.post('/addAgency',upload.single('Logo'),addAgency);
 router.post('/loginAgency',loginAgencyOwner);   
-router.get('/getAgencyById/:id',getAgencyById)
+router.get('/getAgencyById/:id',AuthToken,getAgencyById)
 
 module.exports=router;
